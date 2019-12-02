570f35de43af4a00200179ee80f010c1c73d0662
Leo Antunes <leocrantunes@gmail.com>
2019-12-02 16:42:56 -0500

    Fixed names and event handlers

--- VetClinic.Wpf/ViewModel/AppointmentDialogViewModel.cs
+++ VetClinic.Wpf/ViewModel/AppointmentDialogViewModel.cs
@@ -15,8 +15,8 @@ namespace VetClinic.Wpf.ViewModel
             Schedule = new Schedule();
             RegisteredPets = registeredPets;
 
-            Place = new ObservableCollection<AppointmentPlace>(Enum.GetValues(typeof(AppointmentPlace)).Cast<AppointmentPlace>());
-            TypeofService = new ObservableCollection<ServiceType>(Enum.GetValues(typeof(ServiceType)).Cast<ServiceType>());
+            Places = new ObservableCollection<AppointmentPlace>(Enum.GetValues(typeof(AppointmentPlace)).Cast<AppointmentPlace>());
+            ServiceTypes = new ObservableCollection<ServiceType>(Enum.GetValues(typeof(ServiceType)).Cast<ServiceType>());
         }
 
         private string _message;
@@ -43,9 +43,9 @@ namespace VetClinic.Wpf.ViewModel
 
         public ObservableCollection<Pet> RegisteredPets { get; set; }
 
-        public ObservableCollection<AppointmentPlace> Place { get; set; }
+        public ObservableCollection<AppointmentPlace> Places { get; set; }
 
-        public ObservableCollection<ServiceType> TypeofService { get; set; }
+        public ObservableCollection<ServiceType> ServiceTypes { get; set; }
 
         public bool CheckFields()
         {
