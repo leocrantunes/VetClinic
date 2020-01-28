7f55e83fded4ee3830c42a738be35f9b4d788c9d
Xiaolong Li <lixiaolong0328@gmail.com>
2020-01-28 17:28:44 -0500

    Add test of patient test

--- VetClinic.Test/MainWindowViewModelTest.cs
+++ VetClinic.Test/MainWindowViewModelTest.cs
@@ -14,25 +14,57 @@ namespace VetClinic.Test
     {
         MainWindowViewModel fixture;
 
+        Pet patient;
+
         [SetUp]
         public void Init()
         {
             fixture = new MainWindowViewModel();
+
+            patient = new Pet();
+            patient.Name = "Flipper";
+            patient.Owner.FirstName = "John";
+            patient.Owner.LastName = "Smith";
+        }
+
+        [OneTimeSetUp]
+
+        public void TestFixtureInit()
+        {
+            fixture.VetClinic.Patients.Clear();
         }
 
         [Test]
         public void CreatePatientTest()
         {
-            var patient = new Pet();
-            patient.Name = "Flipper";
-            patient.Owner.FirstName = "John";
-            patient.Owner.LastName = "Smith";
-
             fixture.AddPatient(patient);
 
             Assert.IsNotEmpty(fixture.VetClinic.Patients);
             Assert.AreEqual(1, fixture.VetClinic.Patients.Count);
             Assert.AreEqual(patient.Name, fixture.VetClinic.Patients.FirstOrDefault()?.Name);
+        }
+        [Test]
+        public void EditPatientTest()
+        {
+
+            fixture.AddPatient(patient);
+
+            patient.Age = 1;
+            
+            fixture.EditPatient(patient);
+
+            Assert.AreEqual(patient.Age, fixture.VetClinic.Patients.FirstOrDefault()?.Age);
+            
+        }
+
+        [Test]
+        public void RemovePatientTest()
+        {
+            fixture.AddPatient(patient);
+
+            fixture.RemovePatient(patient);
+
+            Assert.IsEmpty(fixture.VetClinic.Patients);
         }
     }
 }
